8387d8adfab785ca415792a29323e9fb0f1c8978
Bilal Aslam <49366524+bilal-07@users.noreply.github.com>
2022-03-23 16:30:17 +0500

    Update SingleColor.js
    Update Changes

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import rgbToHex from "../utils";
+// import rgbToHex from "../utils";
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   // console.log(hexColor);
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
-  const hex = rgbToHex(...rgb);
+//   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
 
   useEffect(() => {
